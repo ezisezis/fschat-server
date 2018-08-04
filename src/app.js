930e493abfe3afe53716c512499d4b4c00a1ba59
Eduards Klavins <azyrock1992@gmail.com>
2018-08-04 18:56:05 +0200

    Added error handling

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const config = require("./configuration.js");
 const logger = require("./logger");
 const utilities = require("./utilities");
 
+const serializeError = require("serialize-error");
 const escapeHtml = require("escape-html");
 const http = require("http");
 const app = require("express")();
@@ -21,72 +22,90 @@ const handleDisconnect = (socket, reason) => {
   socket.emit("connect_failed", { reason });
   socket.disconnect(true);
 };
-
 io.on("connection", socket => {
-  const username = escapeHtml(socket.handshake.query.username);
-  if (!utilities.isUsernameValid(username)) {
-    handleDisconnect(socket, "INVALID_USERNAME");
-  } else if (!socket.handshake.query.username) {
-    handleDisconnect(socket, "NO_USERNAME");
-  } else if (connectedUsersCache.get(`${socket.id}__${username}`)) {
-    handleDisconnect(socket, "ID_TAKEN");
-  } else if (utilities.isUsernameTaken(connectedUsersCache.keys(), username)) {
-    handleDisconnect(socket, "USERNAME_TAKEN");
-  } else {
-    connectedUsersCache.set(`${socket.id}__${username}`, {
-      username,
-      id: socket.id
-    });
-    logger.info("User connected", { username });
-    socket.emit("connect_success");
-    socket.broadcast.emit("user_joined", { username });
-  }
-
-  socket.on("message", data => {
-    connectedUsersCache.ttl(
-      `${socket.id}__${username}`,
-      config.cache.userConnectionTTL,
-      (err, changed) => {
-        if (err || !changed) {
-          logger.error("Failed to update TTL", { socketID: socket.id });
-        }
-      }
-    );
-
-    const escapedMessage = {
-      username: escapeHtml(data.username),
-      message: escapeHtml(data.message)
-    };
-    logger.info("Message received", {
-      content: escapedMessage.message,
-      username: escapedMessage.username
-    });
-    socket.broadcast.emit("message", escapedMessage);
-  });
-
-  socket.on("disconnect", () => {
-    const disconnectedUser = connectedUsersCache.get(
-      `${socket.id}__${username}`
-    );
-    if (disconnectedUser) {
-      logger.info("User left the chat", {
-        username: disconnectedUser.username
+  try {
+    const username = escapeHtml(socket.handshake.query.username);
+    if (!utilities.isUsernameValid(username)) {
+      handleDisconnect(socket, "INVALID_USERNAME");
+    } else if (!socket.handshake.query.username) {
+      handleDisconnect(socket, "NO_USERNAME");
+    } else if (connectedUsersCache.get(`${socket.id}__${username}`)) {
+      handleDisconnect(socket, "ID_TAKEN");
+    } else if (
+      utilities.isUsernameTaken(connectedUsersCache.keys(), username)
+    ) {
+      handleDisconnect(socket, "USERNAME_TAKEN");
+    } else {
+      connectedUsersCache.set(`${socket.id}__${username}`, {
+        username,
+        id: socket.id
       });
-      socket.broadcast.emit("message", {
-        message: `${disconnectedUser.username} left the chat, connection lost.`
-      });
-
-      connectedUsersCache.del(`${socket.id}__${username}`);
+      logger.info("User connected", { username });
+      socket.emit("connect_success");
+      socket.broadcast.emit("user_joined", { username });
     }
-  });
+
+    socket.on("message", data => {
+      connectedUsersCache.ttl(
+        `${socket.id}__${username}`,
+        config.cache.userConnectionTTL,
+        (err, changed) => {
+          if (err || !changed) {
+            logger.error("Failed to update TTL", { socketID: socket.id });
+          }
+        }
+      );
+
+      const escapedMessage = {
+        username: escapeHtml(data.username),
+        message: escapeHtml(data.message)
+      };
+      logger.info("Message received", {
+        content: escapedMessage.message,
+        username: escapedMessage.username
+      });
+      socket.broadcast.emit("message", escapedMessage);
+    });
+
+    socket.on("disconnect", () => {
+      const disconnectedUser = connectedUsersCache.get(
+        `${socket.id}__${username}`
+      );
+      if (disconnectedUser) {
+        logger.info("User left the chat", {
+          username: disconnectedUser.username
+        });
+        socket.broadcast.emit("message", {
+          message: `${
+            disconnectedUser.username
+          } left the chat, connection lost.`
+        });
+
+        connectedUsersCache.del(`${socket.id}__${username}`);
+      }
+    });
+  } catch (error) {
+    logger.error(
+      "Caught unexpected error when handling connection",
+      serializeError(error)
+    );
+    socket.disconnect(true);
+  }
 });
 
 connectedUsersCache.on("expired", (key, value) => {
-  logger.info("Kicked due to inactivity", { username: value.username });
-  io.to(value.id).emit("kicked_inactive");
-  io.emit("message", {
-    message: `${value.username} was disconnected due to inactivity.`
-  });
+  try {
+    logger.info("Kicked due to inactivity", { username: value.username });
+    io.to(value.id).emit("kicked_inactive");
+    io.emit("message", {
+      message: `${value.username} was disconnected due to inactivity.`
+    });
+  } catch (error) {
+    logger.error(
+      "Caught unexpected error when kicking user",
+      serializeError(error)
+    );
+  }
 });
 
 process.on("SIGTERM", () => {
